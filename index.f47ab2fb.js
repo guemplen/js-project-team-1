18f550cae848c17b8a80c350a91fb9f5dbb0928f
guemplen <57450955+guemplen@users.noreply.github.com>
2023-09-15 21:07:43 +0000

    Deploying to gh-pages from @ guemplen/js-project-team-1@1e39e4028060382266b6c28b67aada354b1fba54 🚀

---
+++ index.f47ab2fb.js
@@ -0,0 +1,2 @@
+!function(){var n={};Object.defineProperty(n,"__esModule",{value:!0}),n.default=function(n,e){if(!(n instanceof e))throw new TypeError("Cannot call a class as a function")}}();
+//# sourceMappingURL=index.f47ab2fb.js.map
