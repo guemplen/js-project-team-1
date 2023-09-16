fc6121ef519faa3de653f44abd7d632b86bcc3d3
domanskayelyzaveta <domanskaya_@ukr.net>
2023-09-16 22:48:39 +0200

    hero-section-push

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,22 +1,56 @@
 import axios from 'axios';
+import Swiper from 'swiper';
+import "swiper/swiper-bundle.min.css";
+
+const refs = {
+    swiperContainer: document.querySelector(".swiper-wrapper"),
+};
 
 const url = "https://tasty-treats-backend.p.goit.global/api/events";
 
 async function fetchHeroMasterClass() {
     try {
-        const response = await axios.get(url)
+        const response = await axios.get(url);
         console.log(response);
         const data = response.data;
-        imgCookWebp = data[0].cook.imgWebpUrl;
-        imgDish1Webp = data[0].topic.imgWebpUrl;
-        imgPreviewDish1Webp = data[0].topic.previewWebpUrl;
-        console.log(imgCookWebp);
-        console.log(imgDish1Webp);
-        console.log(imgPreviewDish1Webp);
-    
+        
+        renderHero(data);
+
+        // Инициализируем Swiper здесь, после получения данных
+        const swiper = new Swiper('.swiper-container', {
+            slidesPerView: 3,
+            spaceBetween: 50,
+            centeredSlides: true,
+            direction: 'horizontal',
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            autoplay: {
+                speed: 3000,
+            },
+        });
     } catch (error) {
         console.log(error);
     }
-};
+}
 
 fetchHeroMasterClass();
+
+function renderHero(data) {
+    const markup = data.map(event => {
+        return `<div class="swiper-slide"><img src="${event.cook.imgWebpUrl}" class="hero-photo-slider photo-cook" /></div>
+             <div class="swiper-slide"><img src="${event.topic.previewWebpUrl}" class="hero-photo-slider photo-dish-small" />
+             <h3 class="hero-main-txt-slide">${event.topic.name}</h3>
+             <p class="hero-txt-slide">${event.topic.area}</p></div>
+             <div class="swiper-slide"><img src="${event.topic.imgWebpUrl}" class="hero-photo-slider photo-dish-big" /></div>
+         `;
+    }).join(" ");
+    
+    refs.swiperContainer.innerHTML = markup;
+}
