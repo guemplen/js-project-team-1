c1fed293b16ec07dccd03d95575960d5e477fb41
domanskayelyzaveta <domanskaya_@ukr.net>
2023-09-17 19:03:04 +0200

    hero section creating

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
-import Swiper from 'swiper';
-import "swiper/swiper-bundle.min.css";
+
+
+import $ from 'jquery';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const refs = {
-    swiperContainer: document.querySelector(".swiper-wrapper"),
+    swiperContainer: document.querySelector(".js-swiper-container"),
 };
 
 const url = "https://tasty-treats-backend.p.goit.global/api/events";
@@ -16,25 +21,6 @@ async function fetchHeroMasterClass() {
         
         renderHero(data);
 
-        // Инициализируем Swiper здесь, после получения данных
-        const swiper = new Swiper('.swiper-container', {
-            slidesPerView: 3,
-            spaceBetween: 50,
-            centeredSlides: true,
-            direction: 'horizontal',
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            autoplay: {
-                speed: 3000,
-            },
-        });
     } catch (error) {
         console.log(error);
     }
@@ -44,13 +30,31 @@ fetchHeroMasterClass();
 
 function renderHero(data) {
     const markup = data.map(event => {
-        return `<div class="swiper-slide"><img src="${event.cook.imgWebpUrl}" class="hero-photo-slider photo-cook" /></div>
-             <div class="swiper-slide"><img src="${event.topic.previewWebpUrl}" class="hero-photo-slider photo-dish-small" />
-             <h3 class="hero-main-txt-slide">${event.topic.name}</h3>
-             <p class="hero-txt-slide">${event.topic.area}</p></div>
-             <div class="swiper-slide"><img src="${event.topic.imgWebpUrl}" class="hero-photo-slider photo-dish-big" /></div>
-         `;
+        return `
+         <div class=slide-wrapper>
+<div class="slick-slide hero-cook-ph-div"><img src="${event.cook.imgWebpUrl}" class="photo-cook" width=351 height=456/></div>
+<div class="slick-slide hero-small-ph-div">
+<img src="${event.topic.previewWebpUrl}"class="photo-dish-small" alt ="" width=351px height=456/>
+    <h3 class="hero-main-txt-slide">${event.topic.name}</h3>
+    <p class="hero-txt-slide">${event.topic.area}</p>
+</div>
+<div class="slick-slide"><img src="${event.topic.imgWebpUrl}"class="photo-dish-big" width=351px height=456/></div>
+</div>
+`;
     }).join(" ");
     
-    refs.swiperContainer.innerHTML = markup;
-}
+  
+ refs.swiperContainer.innerHTML = markup;
+
+
+    $(".js-swiper-container").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 300000,
+    dots: true,
+    infinite: true,
+    draggable: true,
+});
+};
