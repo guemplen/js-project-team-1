827bbaa6f4fb60a582f11e46c60cd0dcd8b4c901
domanskayelyzaveta <domanskaya_@ukr.net>
2023-09-18 10:53:58 +0200

    hero-correction-section

--- src/js/hero.js
+++ src/js/hero.js
@@ -32,13 +32,13 @@ function renderHero(data) {
     const markup = data.map(event => {
         return `
          <div class=slide-wrapper>
-<div class="slick-slide hero-cook-ph-div"><img src="${event.cook.imgWebpUrl}" class="photo-cook" width=351 height=456/></div>
+<div class="slick-slide hero-cook-ph-div"><img src="${event.cook.imgWebpUrl}" class="photo-cook" alt =""/></div>
 <div class="slick-slide hero-small-ph-div">
-<img src="${event.topic.previewWebpUrl}"class="photo-dish-small" alt ="" width=351px height=456/>
+<img src="${event.topic.previewWebpUrl}"class="photo-dish-small" alt =""/>
     <h3 class="hero-main-txt-slide">${event.topic.name}</h3>
     <p class="hero-txt-slide">${event.topic.area}</p>
 </div>
-<div class="slick-slide hero-big-ph-div"><img src="${event.topic.imgWebpUrl}"class="photo-dish-big" width=351px height=456/></div>
+<div class="slick-slide hero-big-ph-div"><img src="${event.topic.imgWebpUrl}"class="photo-dish-big" alt =""/></div>
 </div>
 `;
     }).join(" ");
@@ -52,7 +52,7 @@ function renderHero(data) {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 300000,
+        autoplaySpeed: 3000,
         dots: true,
         draggable: true,
     });
