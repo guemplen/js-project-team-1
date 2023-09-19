a3f07623f22f92dc8ff91cf50845de480c1c47f5
domanskayelyzaveta <domanskaya_@ukr.net>
2023-09-19 18:36:02 +0200

    switcher commit save

--- src/js/hero.js
+++ src/js/hero.js
@@ -14,10 +14,12 @@ const url = 'https://tasty-treats-backend.p.goit.global/api/events';
 async function fetchHeroMasterClass() {
   try {
     const response = await axios.get(url);
-    // console.log(response);
     const data = response.data;
 
+
     renderHero(data);
+
+
   } catch (error) {
     console.log(error);
   }
@@ -28,13 +30,12 @@ fetchHeroMasterClass();
 function renderHero(data) {
   const markup = data
     .map(event => {
-      return `
-         <div class=slide-wrapper>
+      return `<div class=slide-wrapper>
 <div class="slick-slide hero-cook-ph-div"><img src="${event.cook.imgWebpUrl}" class="photo-cook" alt =""/></div>
 <div class="slick-slide hero-small-ph-div">
 <img src="${event.topic.previewWebpUrl}"class="photo-dish-small" alt =""/>
-    <h3 class="hero-main-txt-slide">${event.topic.name}</h3>
-    <p class="hero-txt-slide">${event.topic.area}</p>
+<h3 class="hero-main-txt-slide">${event.topic.name}</h3>
+<p class="hero-txt-slide">${event.topic.area}</p>
 </div>
 <div class="slick-slide hero-big-ph-div"><img src="${event.topic.imgWebpUrl}"class="photo-dish-big" alt =""/></div>
 </div>
@@ -50,7 +51,7 @@ function renderHero(data) {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000000,
+    autoplaySpeed: 3000,
     dots: true,
     draggable: true,
   });
