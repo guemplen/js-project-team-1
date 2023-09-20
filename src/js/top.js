d86d4ba5db1125aec167f8be98a25fbab6b878cc
guemplen <guemplen@gmail.com>
2023-09-20 21:30:42 +0200

    Update top & fix modal-video

--- src/js/top.js
+++ src/js/top.js
@@ -1,7 +1,9 @@
 import { TastyTreats_API } from './api';
+import { openModal } from './modal-video';
 
 const api = new TastyTreats_API();
 const ulElement = document.querySelector('.recipes-sidebar-popular-list');
+const recipeListEl = document.querySelector('.recipes-list');
 
 async function renderPopularRecipes() {
   const response = await api.getAllPopularRecipes();
@@ -14,7 +16,7 @@ async function renderPopularRecipes() {
     .slice(0, quantityOfRecipesToRender)
     .map(recipe => {
       return `
-    <li class="recipes-sidebar-popular-item">
+    <li class="popular-modal recipes-sidebar-popular-item" data-id="${recipe._id}">
       <img class="recipes-sidebar-popular-item-img" src="${recipe.preview}" alt="${recipe.title}" width="64" height="64">
       <div class="recipes-sidebar-popular-item-descr">
         <h3 class="popular-subtitle">${recipe.title}</h3>
@@ -29,3 +31,17 @@ async function renderPopularRecipes() {
 }
 
 renderPopularRecipes();
+
+ulElement.addEventListener('click', function (event) {
+  if (event.target.classList.contains('popular-modal')) {
+    const recipeId = event.target.getAttribute('data-id'); // Получение recipeId из атрибута data-id
+    openModal(recipeId);
+  }
+});
+
+recipeListEl.addEventListener('click', function (event) {
+  if (event.target.classList.contains('recipes-list-see-recipe-btn')) {
+    const btnId = event.target.dataset.id;
+    openModal(btnId);
+  }
+});
