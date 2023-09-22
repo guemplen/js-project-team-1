daa5e9100464a31bc5d1de25e5fc84a605a08d21
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-09-22 13:47:40 +0300

    no-scroll

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,5 @@
 //* MODULE HEADER*//
+
 const modalMenu = document.querySelector('.backdrop-burger');
 const openMenuBtn = document.querySelector('.js-open-menu-burger');
 const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -6,12 +7,14 @@ const closeMenuBtn = document.querySelector('.js-close-menu');
 function openModal() {
   modalMenu.style.visibility = 'visible';
   modalMenu.style.left = '0';
+  document.body.classList.add('body-no-scroll');
 }
 function closeModal() {
   modalMenu.style.left = '-100%';
   setTimeout(() => {
     modalMenu.style.visibility = 'hidden';
   }, 800);
+  document.body.classList.remove('body-no-scroll');
 }
 openMenuBtn.addEventListener('click', openModal);
 closeMenuBtn.addEventListener('click', closeModal);
